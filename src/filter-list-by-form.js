09c0c4d06020871a3b0f99f62f97b83db32e538d
Georg Jähnig <georg@jaehnig.org>
2022-07-17 03:23:11 +0200

    Display.

--- src/filter-list-by-form.js
+++ src/filter-list-by-form.js
@@ -108,7 +108,7 @@ class FilterListByForm {
 
   displayAllLi() {
     this.itemNodes.forEach((liNode) => {
-      liNode.style.display = 'table-cell';
+      liNode.style.display = 'table-row';
     });
   }
 
